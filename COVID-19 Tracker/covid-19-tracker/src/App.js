3bba534fefb650cbd1dc9c054dcb941142b4746a
Nikhil <nikhilbanuri4@gmail.com>
2020-09-27 14:26:55 +0530

    First commit
    Initial commit

---
+++ COVID-19 Tracker/covid-19-tracker/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lets build a COVID19 Tracker</h1>
+
+      {/* Header*/}
+      {/* Title + Select input dropdown field */}
+
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+
+      {/* Tables */}
+      {/* Graph */}
+
+      {/* Maps */}
+    </div>
+  );
+}
+
+export default App;
